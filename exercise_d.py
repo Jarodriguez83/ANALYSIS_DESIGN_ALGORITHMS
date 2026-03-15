c6de445fd31ccecc401fbe6ffbbf0d93a74fe528
Jarodriguez83 <jarodriguez83@ucatolica.edu.co>
2026-03-14 23:44:56 -0500

    EXERCISE D WITH PROFILING

--- exercise_d.py
+++ exercise_d.py
@@ -1,3 +1,4 @@
+import cProfile
 # FUNCION PARA CALCULAR LOS DIVISORES PROPIOS DE UN NUMERO
 def divisores_propios(n):
     divisores = []
@@ -11,66 +12,69 @@ def suma_divisores(n):
     divisores = divisores_propios(n)
     return sum(divisores)
 
-N = int(input(" - INGRESE UN NUMERO ENTERO POSITIVO N: "))
+def main():
+    N = int(input(" - INGRESE UN NUMERO ENTERO POSITIVO N: "))
 
-# CONTADORES
-cantidad_deficientes = 0
-cantidad_perfectos = 0
-cantidad_abundantes = 0
+    # CONTADORES
+    cantidad_deficientes = 0
+    cantidad_perfectos = 0
+    cantidad_abundantes = 0
 
-numeros_perfectos = [] # LISTA PARA GUARDAR NUMEROS PERFECTOS
+    numeros_perfectos = [] # LISTA PARA GUARDAR NUMEROS PERFECTOS
 
-sumas = {} # DICCIONARIO PARA GUARDAR s(n)
+    sumas = {} # DICCIONARIO PARA GUARDAR s(n)
 
-print("\n   ANÁLISIS DE NUMEROS\n")
+    print("\n   ANÁLISIS DE NUMEROS\n")
 
-# RECORRER NUMEROS DESDE 1 HASTA N
-for n in range(1, N + 1):
-    divisores = divisores_propios(n)
-    s = sum(divisores)
-    # GUARDAR LA SUMA PARA BUSCAR NUMEROS AMIGOS
-    sumas[n] = s
-    # MOSTRAR DIVISORES
-    print("- NUMERO:", n)
-    print("- DIVISORES PROPIOS:", divisores)
-    print("- SUMA DE DIVISORES:", s)
+    # RECORRER NUMEROS DESDE 1 HASTA N
+    for n in range(1, N + 1):
+        divisores = divisores_propios(n)
+        s = sum(divisores)
+        # GUARDAR LA SUMA PARA BUSCAR NUMEROS AMIGOS
+        sumas[n] = s
+        # MOSTRAR DIVISORES
+        print("- NUMERO:", n)
+        print("- DIVISORES PROPIOS:", divisores)
+        print("- SUMA DE DIVISORES:", s)
 
-    # CLASIFICACION
-    if s == n:
-        print(" - CLASIFICACION: PERFECTO\n")
-        cantidad_perfectos += 1
-        numeros_perfectos.append(n)
+        # CLASIFICACION
+        if s == n:
+            print(" - CLASIFICACION: PERFECTO\n")
+            cantidad_perfectos += 1
+            numeros_perfectos.append(n)
 
-    elif s < n:
-        print(" - CLASIFICACION: DEFICIENTE\n")
-        cantidad_deficientes += 1
+        elif s < n:
+            print(" - CLASIFICACION: DEFICIENTE\n")
+            cantidad_deficientes += 1
 
+        else:
+            print(" - CLASIFICACION: ABUNDANTE\n")
+            cantidad_abundantes += 1
+
+    # BUSQUEDA DE NUMEROS AMIGOS
+    pares_amigos = []
+    for a in range(1, N + 1):
+        b = sumas[a]
+
+        if b > a and b <= N:
+            if sumas.get(b, 0) == a:
+                pares_amigos.append((a, b))
+
+    # RESULTADOS FINALES
+    print("\n  RESULTADOS FINALES   \n")
+    print("- NUMEROS PERFECTOS ENCONTRADOS:")
+    print(numeros_perfectos)
+
+    print("\n- CANTIDAD DE NUMEROS:")
+    print("- DEFICIENTES:", cantidad_deficientes)
+    print("- PERFECTOS:", cantidad_perfectos)
+    print("- ABUNDANTES:", cantidad_abundantes)
+
+    print("\n- PARES DE NUMEROS AMIGOS ENCONTRADOS:")
+    if len(pares_amigos) == 0:
+        print(" ALERTA: NO SE ENCONTRARON PARES AMIGOS")
     else:
-        print(" - CLASIFICACION: ABUNDANTE\n")
-        cantidad_abundantes += 1
+        for par in pares_amigos:
+            print(par)
 
-# BUSQUEDA DE NUMEROS AMIGOS
-pares_amigos = []
-for a in range(1, N + 1):
-    b = sumas[a]
-
-    if b > a and b <= N:
-        if sumas.get(b, 0) == a:
-            pares_amigos.append((a, b))
-
-# RESULTADOS FINALES
-print("\n  RESULTADOS FINALES   \n")
-print("- NUMEROS PERFECTOS ENCONTRADOS:")
-print(numeros_perfectos)
-
-print("\n- CANTIDAD DE NUMEROS:")
-print("- DEFICIENTES:", cantidad_deficientes)
-print("- PERFECTOS:", cantidad_perfectos)
-print("- ABUNDANTES:", cantidad_abundantes)
-
-print("\n- PARES DE NUMEROS AMIGOS ENCONTRADOS:")
-if len(pares_amigos) == 0:
-    print(" ALERTA: NO SE ENCONTRARON PARES AMIGOS")
-else:
-    for par in pares_amigos:
-        print(par)
+cProfile.run('main()')
